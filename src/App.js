91a32483d75b042cdbbbc7ecfff6769d841a4320
Fahad <puru2895@gmail.com>
2024-09-02 21:12:36 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -15,17 +15,19 @@ function App() {
 
   function handlePrevious() {
     // alert("previous");
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
 
   function handleNext() {
     // alert("Next");
 
-    if (step < 3) setStep(step + 1);
+    // if (step < 3) setStep(step + 1);
+    //callback for updating the state with the current state
+    if (step < 3) setStep((s) => s + 1);
   }
   return (
     <>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
       {isOpen && (
